4d15ad353c0b46206bfacee93aefbda20ff654d6
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-08-06 21:34:26 -0300

    Adicionar somente números ao botão de multiplicar

--- Calculadora.cs
+++ Calculadora.cs
@@ -9,12 +9,10 @@ namespace Calculadora
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
-            //Validação de Campos
             if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
             {
-                MessageBox.Show("Por favor, preencha ambos os números antes de somar.",
-                                "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return; // Para a execução se algum campo estiver vazio
+                MessageBox.Show("Por favor, preencha ambos os números antes de somar.");
+                return; 
             }
 
             double numero1, numero2;
@@ -24,12 +22,11 @@ namespace Calculadora
 
             if (!numero1Valido || !numero2Valido)
             {
-                MessageBox.Show("Digite apenas números válidos nos campos.",
-                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Digite apenas números válidos nos campos.");
                 return;
             }
 
-            // Soma e exibe o resultado
+           
             lblResultado.Text = (numero1 + numero2).ToString();
 
         }
@@ -37,12 +34,11 @@ namespace Calculadora
         private void btnSubtrair_Click(object sender, EventArgs e)
         {
 
-            //Validação de Campos
+           
             if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
             {
-                MessageBox.Show("Por favor, preencha ambos os números antes de subtrair.",
-                                "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return; // Para a execução se algum campo estiver vazio
+                MessageBox.Show("Por favor, preencha ambos os números antes de subtrair.");
+                return; 
             }
 
             double numero1, numero2;
@@ -52,32 +48,65 @@ namespace Calculadora
 
             if (!numero1Valido || !numero2Valido)
             {
-                MessageBox.Show("Digite apenas números válidos nos campos.",
-                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Digite apenas números válidos nos campos.");
                 return;
             }
 
-            // Soma e exibe o resultado
-            lblResultado.Text = (numero1 + numero2).ToString();
+           
+            lblResultado.Text = (numero1 - numero2).ToString();
 
 
         }
 
         private void btnMultiplicar_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
 
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("Por favor, preencha ambos os números antes de multiplicar.");
+                return;
+
+            }
+            double numero1, numero2;
+
+            bool numero1Valido = double.TryParse(txtNumero1.Text, out numero1);
+            bool numero2Valido = double.TryParse(txtNumero2.Text, out numero2);
+
+            if (!numero1Valido || !numero2Valido)
+            {
+                MessageBox.Show("Digite apenas números válidos nos campos.");
+                return;
+            }
+
+           
             lblResultado.Text = (numero1 * numero2).ToString();
+
         }
 
         private void btnDividir_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("Por favor, preencha ambos os números antes de dividir.");
+                return;
+
+            }
+            double numero1, numero2;
+
+            bool numero1Valido = double.TryParse(txtNumero1.Text, out numero1);
+            bool numero2Valido = double.TryParse(txtNumero2.Text, out numero2);
+
+            if (!numero1Valido || !numero2Valido)
+            {
+                MessageBox.Show("Digite apenas números válidos nos campos.");
+                return;
+            }
+
 
             lblResultado.Text = (numero1 / numero2).ToString();
+
         }
+        
 
         private void lblNumero1_Click(object sender, EventArgs e)
         {
